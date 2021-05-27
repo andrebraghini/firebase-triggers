bc1f34585b578c50cc33cdc450b5c40cea6ca3a5
Andre Braghini <dedoxbr@gmail.com>
2021-05-27 00:13:54 -0300

    chore: update packages

--- src/lib/handlers/request-schema-validator-handler.ts
+++ src/lib/handlers/request-schema-validator-handler.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
-import * as Ajv from 'ajv';
+import Ajv from 'ajv';
 import { readFileSync } from 'fs';
 import { SCHEMA_INVALID } from '../errors';
 
